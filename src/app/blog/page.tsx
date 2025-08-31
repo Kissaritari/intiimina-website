9e78e7706b722e3fb557ae105d54571b40fe2864
Kissaritari <tiitiainnin@hotmail.com>
2025-08-31 19:16:15 +0300

    modified blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -41,7 +41,7 @@ export default async function Blog() {
       <main className="max-w-4xl mx-auto py-16 px-4">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold mb-4">Blogi</h1>
-          <p className="text-xl text-gray-600 mb-8">Ajatuksia ja oivalluksia seksuaalisuudesta, sukupuolesta ja moninaisuudesta</p>
+          <p className="text-xl text-gray-600 mb-8">Ajatuksia ja oivalluksia hyvinvoinnista, moninaisuudesta ja rohkeudesta.</p>
         </div>
 
         <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
